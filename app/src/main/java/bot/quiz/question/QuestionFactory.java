98b0f1ef15b2cf4fe42d9110e635d33acac1f92b
Christolis <work@chris-sdogkos.com>
2023-07-11 14:25:32 +0300

    feat: make grade ranges match and add small hint

--- app/src/main/java/bot/quiz/question/QuestionFactory.java
+++ app/src/main/java/bot/quiz/question/QuestionFactory.java
@@ -22,26 +22,13 @@ public class QuestionFactory {
     public FlashcardQuestion createFlashcardQuestion(int id, JournalWord word) {
         final FlashcardQuestion question = new FlashcardQuestion(id, word);
 
-        // Construct question embed
-        EmbedBuilder questionEmbed = new EmbedBuilder();
-        questionEmbed.setAuthor("Question #" + id);
-        questionEmbed.setTitle("What is the definition of the word \"" + word + "\"? 🤔");
-        questionEmbed.addField(
-                "",
-                "Try to guess the meaning of the"
-                        + " word and after you think you got it, click on the"
-                        + " **Reveal Answer** below.",
-                false);
-        questionEmbed.setColor(Constants.EMBED_COLOR);
-        question.setQuestion(questionEmbed.build());
-
         // Construct answer embed
         EmbedBuilder answerEmbed = new EmbedBuilder();
         answerEmbed.setAuthor("Question #" + id);
         answerEmbed.setTitle("📖 The definition of \"" + word + "\" is:");
 
         Optional<CachedWord.Definition> definitionOptional =
-                wordsService.getDefinitionByIndex(word.getWord(), word.getDefinitionIndex());
+            wordsService.getDefinitionByIndex(word.getWord(), word.getDefinitionIndex());
 
         if (definitionOptional.isEmpty()) {
             answerEmbed.addField("", "Well, this is awkward, but there is no definition!", true);
@@ -53,12 +40,27 @@ public class QuestionFactory {
         answerEmbed.addField(definition.getPartOfSpeech(), "> " + definition.getDefinition(), true);
 
         answerEmbed.addField(
+            "",
+            "On a scale of 1️⃣ to 5️⃣, click on the buttons below to"
+                + " indicate how well you guessed the definition of the word.\n\n"
+                + "- 1️⃣: *Your mind completely went blank over the definition.*\n"
+                + "- 5️⃣: *You remembered the word's definition perfectly!*",
+            false);
+
+        // Construct question embed
+        EmbedBuilder questionEmbed = new EmbedBuilder();
+        String part = definition.getPartOfSpeech();
+        questionEmbed.setAuthor("Question #" + id);
+        questionEmbed.setTitle("What is the definition of the word \"" + word + "\" (" + part + ")? 🤔");
+        questionEmbed.addField(
                 "",
-                "On a scale of 0️⃣ to 5️⃣, click on the buttons below to"
-                        + " indicate how well you guessed the definition of the word.\n\n"
-                        + "- 0️⃣: *Your mind completely went blank over the definition.*\n"
-                        + "- 5️⃣: *You remembered the word's definition perfectly!*",
+                "Try to guess the meaning of the"
+                        + " word and after you think you got it, click on the"
+                        + " **Reveal Answer** below.",
                 false);
+        questionEmbed.setColor(Constants.EMBED_COLOR);
+        question.setQuestion(questionEmbed.build());
+
         question.setAnswer(answerEmbed.build());
         return question;
     }
