09c6dd83ca594e12472583baa225b8c16ff997ac
Atakan <atakan.kumas03@gmail.com>
2025-04-17 17:18:44 +0200

    log calls added to joinlistener and static variable member role id defined in constants!

--- app/src/main/java/bot/listener/JoinListener.java
+++ app/src/main/java/bot/listener/JoinListener.java
@@ -1,36 +1,40 @@
 package bot.listener;
 
+import bot.Constants;
+import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.apache.logging.log4j.Level;
 
 /**
  * @author Eaky0 A listener called whenever a user joins the guild. Mainly used for assigning
  *     initial roles such as 'Member'.
  */
+@Log4j2
 public class JoinListener extends ListenerAdapter {
-
     /**
      * @param event The event that takes place when a User/Member joins the guild.
      */
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
-        User user = event.getUser();
         try {
-            // 505348918520053760L Member role id in the Official Learn English Server.
-            // 1120239610920976563L Member role id in the Learn English (Staging) Server.
-            Role role = event.getGuild().getRoleById(505348918520053760L);
+            User user = event.getUser();
+            Role role = event.getGuild().getRoleById(Constants.MEMBER_ROLE_ID);
             if (role != null) {
                 // Assign the member role to the new user.
                 event.getGuild().addRoleToMember(user, role).queue();
+
+                String logNote = "Successfully assigned the member role to " + user.getName() + "!";
+                log.log(Level.INFO, logNote);
+
             } else {
                 throw new NullPointerException();
             }
 
         } catch (NullPointerException e) {
-            System.out.println("This role doesn't exist!");
-            return;
+            log.error("Role doesn't exist!");
         }
     }
 }
